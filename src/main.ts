91e18cd919b0967b4a1d33a9cafb0ec083125dda
nano1237 <tim.ruecker@ymail.com>
2018-11-09 21:52:14 +0100

    adding a autocomplete component to the project and cleaning the data folder up

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import {ScrollTopComponent} from "./examples/scroll-top.component";
-import {ScrollToTargetComponent} from "./examples/scroll-to-target.component";
+import {ScrollTopComponent} from "./examples/scroll/scroll-top.component";
+import {ScrollToTargetComponent} from "./examples/scroll/scroll-to-target.component";
 import {AccordionComponent} from "./examples/accordion.component";
-import {ModalComponent} from "./examples/modal.component";
-
+import {ModalComponent} from "./examples/modal/modal.component";
+import "./events/key.events";
+import {ModalTriggerComponent} from "./examples/modal/modal-trigger.component";
+import {AutoCompleteComponent} from "./examples/forms/auto-complete.component";
 
 new ScrollTopComponent();
 new ScrollToTargetComponent();
 new AccordionComponent();
 new ModalComponent();
+new ModalTriggerComponent();
+new AutoCompleteComponent();
