b399469cb71460e554b5a458ffb892bb40f2fcda
Tim Rücker <tim.ruecker@wendweb.de>
2018-11-27 16:09:32 +0100

    adding method for template rendering

--- src/classes/template.ts
+++ src/classes/template.ts
@@ -5,4 +5,17 @@ export class Template {
 	get html(): string {
 		return this._html;
 	}
+
+	public render(vars: any) {
+		let html = this.html;
+		for (let name in vars) {
+			if (vars.hasOwnProperty(name)) {
+				let regex = new RegExp('\\${' + name + '}', 'g');
+
+				html = html.replace(regex, vars[name]);
+			}
+		}
+
+		return html;
+	}
 }
