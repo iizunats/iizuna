589f5bddf19ebee9cc0b0a316d352077ed2a9fcc
nano1237 <tim.ruecker@ymail.com>
2018-11-29 21:11:31 +0100

    writing test case for debounce decorator

--- src/decorators/debounce.decorator.spec.ts
+++ src/decorators/debounce.decorator.spec.ts
@@ -7,4 +7,22 @@ describe('Debounce Decorator', () => {
 	it('should be declared and be accessible from anywhere', () => {
 		expect(typeof Debounce).to.equal('function');
 	});
+
+	it('should add a debounce to a function', () => {
+		let debounceDecorator = Debounce(400);
+		let obj = {
+			val: 0,
+			test: function () {
+				this.val = 1;
+			}
+		};
+		debounceDecorator(obj, 'test');
+
+		expect(obj.val).to.equal(0);
+		obj.test();
+		expect(obj.val).to.equal(0);
+		setTimeout(function () {
+			expect(obj.val).to.equal(1);
+		}, 500);
+	});
 });
