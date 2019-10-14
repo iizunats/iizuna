614c4a995f1116a31d6f744f30b9cecb211c57ce
nano1237 <tim.ruecker@ymail.com>
2019-10-14 23:34:37 +0200

    fixing generics

--- src/facades/caching/caching-facade.interface.ts
+++ src/facades/caching/caching-facade.interface.ts
@@ -2,5 +2,5 @@ import {Promise} from "es6-promise";
 
 export interface CachingFacadeInterface<KEY, VALUE> {
 	set: (key: KEY, value: VALUE) => Promise<void>;
-	get: (key: KEY) => Promise<VALUE>;
+	get: <VALUE>(key: KEY) => Promise<VALUE>;
 }
